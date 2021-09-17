d51fbe26f35ea30b8db47d45f82d18072bb9f75d
Bhpsngum <bhpsngumproductions@gmail.com>
2021-09-17 18:47:21 +0900

    hotfix

--- getProperVariableName/JS/getProperVariableName.js
+++ getProperVariableName/JS/getProperVariableName.js
@@ -105,7 +105,7 @@ String.prototype.getProperJSVariableName = function getProperJSVariableName()
   let opt={name:inp,mutable:!(temp == window[inp]||inp == "NaN"),proper:this+""==inp};
   if (!opt.mutable)
   {
-    if (!arguments[1]) console.warn(`"'"+inp+"' is initially defined as a global variable or the properties of them, therefore it's immutable or read-only. Setting them won’t have an effect. Avoid using this input as a variable name.");
+    if (!arguments[1]) console.warn("'"+inp+"' is initially defined as a global variable or the properties of them, therefore it's immutable or read-only. Setting them won’t have an effect. Avoid using this input as a variable name.");
     else
     {
       opt.name = "_"+opt.name;
